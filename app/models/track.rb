64571f00e18635ca43a49870b2338d432840b4ad
Dillon Luther <dillontluther@gmail.com>
2017-09-21 09:40:24 -0700

    Removing plays from tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -4,8 +4,8 @@ class Track < ApplicationRecord
     foreign_key: :user_id,
     class_name: :User
   has_many :comments
-  has_many :plays
-  has_many :user_listens,
-    through: plays,
-    source: users
+  # has_many :plays
+  # has_many :user_listens,
+  #   through: plays,
+  #   source: users
 end
