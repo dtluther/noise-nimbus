a3998ffc10f80ccddd8c301075ad440f70fd87df
Dillon Luther <dillontluther@gmail.com>
2017-09-25 19:24:10 -0700

    removing react menu packages

--- frontend/components/play_bar/play_bar.jsx
+++ frontend/components/play_bar/play_bar.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+
 
 class PlayBar extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      onPlay: false,
+      onPause: false,
+      onEnded: false
+    };
+  }
 
 
   render() {
     return (
-      <div className="play-bar">
+      <div className="play-bar-div">
         <h1>
           I am the play bar in the footer!!!
         </h1>
+
+        <ReactAudioPlayer className="play-bar"
+          src="https://s3-us-west-1.amazonaws.com/noise-nimbus-dev/tracks/track_uploads/000/000/006/original/mindmapthat_-_Transmutation.mp3"
+          controls="true"
+          volume="0.5"
+
+          >
+
+        </ReactAudioPlayer>
       </div>
     );
   }
